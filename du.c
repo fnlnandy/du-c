d38340180e2e5a194221fc84f26c408f68d01876
fnlnandy <andyfaniloniaina01@gmail.com>
2024-06-29 20:45:23 +0300

    Removed braces from if...else statemens since they were useless after all.

--- du.c
+++ du.c
@@ -22,28 +22,18 @@ int main(int argc, char **argv)
     for (int i = 0; i < argc; i++)
     {
         if (strncmp(argv[i], "-b", 2) == STR_EQ)
-        {
             forcedType = BYTE;
-        }
         else if (strncmp(argv[i], "-k", 2) == STR_EQ)
-        {
             forcedType = KILOBYTE;
-        }
         else if (strncmp(argv[i], "-m", 2) == STR_EQ)
-        {
             forcedType = MEGABYTE;
-        }
         else if (strncmp(argv[i], "-g", 2) == STR_EQ)
-        {
             forcedType = GIGABYTE;
-        }
         // If this string is not a default argument,
         // and not an argument, then we got our
         // filename supposedly.
         else if (strncmp(argv[i], "-", 1) != STR_EQ)
-        {
             filePath = argv[i];
-        }
     }
 
     displayFileSize(filePath, forcedType);
