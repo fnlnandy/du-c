7516fd2766b4ad773bd1066359b8f6ce6ac83758
fnlnandy <andyfaniloniaina01@gmail.com>
2024-06-29 19:29:31 +0300

    Added FATAL macro.

--- du.h
+++ du.h
@@ -3,4 +3,10 @@
 
 #define UNUSED(var) (void)(var)
 
+#define FATAL(fmt, ...)                    \
+    {                                      \
+        fprintf(stderr, fmt, __VA_ARGS__); \
+        exit(1);                           \
+    }
+
 #endif // GUARD_DU_H_INCLUDED
